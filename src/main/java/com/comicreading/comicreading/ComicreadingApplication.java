daa6f103fc85e0faf9e7c9fe9495961150693267
Johann Jungbauer <jungbauer@gmail.com>
2022-12-02 17:08:21 +0200

    Initial spring boot app

---
+++ src/main/java/com/comicreading/comicreading/ComicreadingApplication.java
@@ -0,0 +1,13 @@
+package com.comicreading.comicreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicreadingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comicreading/comicreading/ComicreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comicreading.comicreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
