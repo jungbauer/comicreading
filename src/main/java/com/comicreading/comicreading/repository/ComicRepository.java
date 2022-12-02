de73781cc1f88dc089941626dbcd1c8772116c88
Johann Jungbauer <jungbauer@gmail.com>
2022-12-02 19:47:56 +0200

    feat: adds repo class for Comic

---
+++ src/main/java/com/comicreading/comicreading/repository/ComicRepository.java
@@ -0,0 +1,11 @@
+package com.comicreading.comicreading.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.comicreading.comicreading.domain.Comic;
+
+@Repository
+public interface ComicRepository extends CrudRepository<Comic, Integer>{
+    
+}
