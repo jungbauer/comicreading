9bb208f23b9e3899494d451cf5708192ac4fd5a1
Johann Jungbauer <jungbauer@gmail.com>
2022-12-21 01:48:26 +0200

    feat: adds ComicService class

--- src/main/java/com/comicreading/comicreading/controller/ComicController.java
+++ src/main/java/com/comicreading/comicreading/controller/ComicController.java
@@ -1,6 +1,5 @@
 package com.comicreading.comicreading.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,31 +8,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.comicreading.comicreading.domain.Comic;
-import com.comicreading.comicreading.repository.ComicRepository;
+import com.comicreading.comicreading.service.ComicService;
 
 @Controller
 public class ComicController {
     
     @Autowired
-    private ComicRepository comicRepository;
+    private ComicService comicService;
 
     @GetMapping("/comics")
     public String comicList(Model model) {
-        List<Comic> list = new ArrayList<>();
-        for (Comic comic : comicRepository.findAll()) {
-            list.add(comic);
-        }
+        List<Comic> list = comicService.getAllComics();
         model.addAttribute("comics", list);
         return "comic/comicList";
     }
-
-    // @GetMapping("/comicinit")
-    // public String comicInit(Model model) {
-
-    //     comicRepository.save(new Comic("The Beginning After the End", "https://toonily.net/manga/the-beginning-after-the-end/", "171"));
-	// 	comicRepository.save(new Comic("Mercenary Enrollment", "https://toonily.net/manga/mercenary-enrollment/", "113"));
-	// 	comicRepository.save(new Comic("The Beginning After the End", "https://toonily.net/manga/reaper-of-the-drifting-moon/", "41"));
-
-    //     return "comic/comicInit";
-    // }
+    
 }
---
+++ src/main/java/com/comicreading/comicreading/service/ComicService.java
@@ -0,0 +1,33 @@
+package com.comicreading.comicreading.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.comicreading.comicreading.domain.Comic;
+import com.comicreading.comicreading.repository.ComicRepository;
+
+@Service
+public class ComicService {
+    
+    @Autowired
+    private ComicRepository comicRepository;
+
+    public List<Comic> getAllComics() {
+        List<Comic> list = new ArrayList<>();
+        for (Comic comic : comicRepository.findAll()) {
+            list.add(comic);
+        }
+
+        return list;
+    }
+
+    public void addInitialTestComics() {
+        comicRepository.save(new Comic("The Beginning After the End", "https://toonily.net/manga/the-beginning-after-the-end/", "171"));
+		comicRepository.save(new Comic("Mercenary Enrollment", "https://toonily.net/manga/mercenary-enrollment/", "113"));
+		comicRepository.save(new Comic("The Beginning After the End", "https://toonily.net/manga/reaper-of-the-drifting-moon/", "41"));
+    }
+    
+}
