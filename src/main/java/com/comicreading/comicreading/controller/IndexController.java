802488b304b99faa6ad579e6ef5f82bce62e1350
Johann Jungbauer <jungbauer@gmail.com>
2022-12-21 01:51:23 +0200

    feat: redirect index page to comic list page

--- src/main/java/com/comicreading/comicreading/controller/IndexController.java
+++ src/main/java/com/comicreading/comicreading/controller/IndexController.java
@@ -1,13 +1,25 @@
 package com.comicreading.comicreading.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.comicreading.comicreading.domain.Comic;
+import com.comicreading.comicreading.service.ComicService;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private ComicService comicService;
     
     @GetMapping("/")
-    public String homePage() {
-        return "index";
+    public String homePage(Model model) {
+        List<Comic> list = comicService.getAllComics();
+        model.addAttribute("comics", list);
+        return "comic/comicList";
     }
 }
