613d7d06f24bf446df0d7b7fe05c62243d0ee681
Johann Jungbauer <jungbauer@gmail.com>
2022-12-23 19:51:02 +0200

    refactor: applies recommended edit
    Removes the repository tag with seems to be unnecessary.

--- src/main/java/com/comicreading/comicreading/repository/ComicRepository.java
+++ src/main/java/com/comicreading/comicreading/repository/ComicRepository.java
@@ -1,11 +1,9 @@
 package com.comicreading.comicreading.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.comicreading.comicreading.domain.Comic;
 
-@Repository
 public interface ComicRepository extends JpaRepository<Comic, Integer>{
     
     Comic findOneByTitle(String title);
