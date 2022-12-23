baf51b7dc040a3c6d691d553aff973c1687774c4
Johann Jungbauer <jungbauer@gmail.com>
2022-12-23 23:50:11 +0200

    feat: redirects incComic based on referer

--- src/main/java/com/comicreading/comicreading/controller/ComicController.java
+++ src/main/java/com/comicreading/comicreading/controller/ComicController.java
@@ -3,11 +3,13 @@ package com.comicreading.comicreading.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -54,12 +56,15 @@ public class ComicController {
     }
 
     @GetMapping("/incComic")
-    public ModelAndView incComic(@RequestParam(name = "id") String comicId, Model model) {
+    public ModelAndView incComic(@RequestParam(name = "id") String comicId, Model model, 
+    @RequestHeader(value = HttpHeaders.REFERER, required = false) String referer) {
         try {
             Comic editComic = comicService.findComicById(Integer.parseInt(comicId));
             editComic.incrementChapter();
             comicService.saveComic(editComic);
-            return new ModelAndView("redirect:comics");
+            if(referer.substring(referer.length()-6, referer.length()).equals("comics"))
+                return new ModelAndView("redirect:comics");
+            else return new ModelAndView("redirect:/");
         } catch (Exception e) {
             // TODO: handle exception
             return new ModelAndView("error");
