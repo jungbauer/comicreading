179925890fba7750234f03e36be228cd66c0695e
Johann Jungbauer <jungbauer@gmail.com>
2023-02-09 19:40:50 +0200

    feat: limits Demo users to 5 comics

--- src/main/java/com/comicreading/controller/ComicController.java
+++ src/main/java/com/comicreading/controller/ComicController.java
@@ -3,6 +3,7 @@ package com.comicreading.controller;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,7 +44,15 @@ public class ComicController {
     }
 
     @GetMapping("/addComic")
-    public String comicForm(Model model) {
+    public String comicForm(Model model, Authentication authentication) {
+        if (authentication != null && authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_DEMO"))) {
+            User user = userService.getUserFromEmail(authentication.getName());
+            if (user.getComics().size() >= 5) {
+                model.addAttribute("message", "Demo users can't have more than 5 comics.");
+                return "error";
+            }
+        }
+
         model.addAttribute("comic", new Comic());
         return "comic/comicForm";
     }
