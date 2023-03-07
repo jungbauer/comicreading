2e71e2c61926331d4b1383644ea92eb81296d8bb
Johann Jungbauer <jungbauer@gmail.com>
2023-03-07 18:52:17 +0200

    feat: adds terminal logging into db logging service

--- src/main/java/com/comicreading/service/DatabaseLogsService.java
+++ src/main/java/com/comicreading/service/DatabaseLogsService.java
@@ -2,16 +2,19 @@ package com.comicreading.service;
 
 import com.comicreading.domain.DatabaseLogs;
 import com.comicreading.repository.DatabaseLogsRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class DatabaseLogsService {
 
     @Autowired
     private DatabaseLogsRepository databaseLogsRepository;
 
     public void logMessage(String message) {
+        log.debug(message);
         DatabaseLogs log = new DatabaseLogs(message);
         databaseLogsRepository.save(log);
     }
