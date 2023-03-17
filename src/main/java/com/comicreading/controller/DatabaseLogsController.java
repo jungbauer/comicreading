391a24e69a85da788c3a8bb1bd21a5ec27b28cb0
Johann Jungbauer <jungbauer@gmail.com>
2023-03-17 20:33:47 +0200

    feat: adds UI ability to delete a log message

--- src/main/java/com/comicreading/controller/DatabaseLogsController.java
+++ src/main/java/com/comicreading/controller/DatabaseLogsController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.comicreading.service.DatabaseLogsService;
 import com.comicreading.domain.DatabaseLogs;
@@ -25,6 +27,19 @@ public class DatabaseLogsController {
         List<DatabaseLogs> logList = databaseLogsService.getAllLogs();
         model.addAttribute("logs", logList);
         return "admin/logs";
+    }
+
+    @GetMapping("/admin/deleteLog")
+    public ModelAndView deleteLog(@RequestParam(name = "id") String logId, Model model, Principal principal) {
+        //TODO this really needs a confirmation modal etc
+        try {
+            databaseLogsService.deleteLog(Integer.parseInt(logId));
+            return new ModelAndView("redirect:/admin/logs");
+        } catch (Exception e) {
+            log.error("Error when deleting comic.", e);
+            return new ModelAndView("error");
+        }
+        
     }
     
 }
--- src/main/java/com/comicreading/service/DatabaseLogsService.java
+++ src/main/java/com/comicreading/service/DatabaseLogsService.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -33,5 +34,12 @@ public class DatabaseLogsService {
         list.sort(Comparator.comparing(DatabaseLogs::getLogDateTime).reversed());
 
         return list;
+    }
+
+    public void deleteLog(Integer id) throws Exception{
+        if(id == null) throw new Exception("Id should not be null");
+        Optional<DatabaseLogs> optLog = databaseLogsRepository.findById(id);
+        if(optLog.isPresent()) databaseLogsRepository.delete(optLog.get());
+        else throw new Exception("log not found");
     }
 }
